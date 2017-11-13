1afa8b7fd3a65ba7558ed4d5638a89cec10d91d2
Chaim Zushe Wilmowsky <chaimzushe@gmail.com>
2017-11-13 14:26:59 -0500

    added checker if game is over

--- lib/board.jsx
+++ lib/board.jsx
@@ -4,8 +4,21 @@ import ReactDOM from 'react-dom';
 class Board extends React.Component{
 
   constructor(){
+
     super()
+    this.coord_map = {
+      0: [0,0],
+      1: [0,1],
+      2: [0,2],
+      3: [1,0],
+      4: [1,1],
+      5: [1,2],
+      6: [2,0],
+      7: [2,1],
+      8: [2,2]
+    }
     this.state = {
+      innerBoard: [[null,null,null], [null,null,null], [null,null,null] ],
       squares: [" ", " ", " "," "," "," "," "," "," "],
     }
     this.mark =  "x"
@@ -14,8 +27,20 @@ class Board extends React.Component{
   handleClick(e){
 
     if(e.target.innerText != "") return;
+    let pos = this.coord_map[e.target.className[0]];
+    this.state.innerBoard[pos[0]][pos[1]] = this.mark;
     e.target.innerText = this.mark;
     this.mark = (this.mark == 'x' ? 'o' : 'x');
+    this.isOver()
+  }
+
+  isWon(){
+
+  }
+
+  isOver(){
+     let result = [].concat.apply([], this.state.innerBoard);
+     console.log(result.every( sqr => sqr != null));
   }
 
   render(){
@@ -27,7 +52,7 @@ class Board extends React.Component{
         return <li
           key={`${sqr, i}`}
           id="sqr"
-          className={`sqr:${sqr}`}
+          className={`${i}:sqr`}
           onClick={this.handleClick.bind(this)}>
           <p>{this.state.squares[i]}</p>
         </li>
